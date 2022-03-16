29786e9a3a50de1d1eeecb69da89c23ece92cd77
FaridKishiyev <99797424+FaridKishiyev@users.noreply.github.com>
2022-03-16 22:37:10 +0400

    users&pasword

---
+++ task2022/task2022/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using task2022.Moduls;
+namespace task2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("Ferid", -28, "123456eri");
+            
+        }
+    }
+}
